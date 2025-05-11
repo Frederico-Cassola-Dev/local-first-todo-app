dc47b992a7abff64a351047c7ab03295f9082419
Frederico Cassola <cfcassola@gmail.com>
2025-05-11 12:36:26 +0200

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI4MDg0NA/spectral.js?branch_slug=main&token=670875b6-1c83-4be7-be17-c1c5119ff989";
+export default { extends: ruleset };
